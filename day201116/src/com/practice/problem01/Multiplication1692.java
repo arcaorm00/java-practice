23a340e94a4434252ead06d1bdf78657c67f1ef9
saltK <arcaorm00@gmail.com>
2020-11-26 20:29:49 +0900

    practiced multiplication

---
+++ day201116/src/com/practice/problem01/Multiplication1692.java
@@ -0,0 +1,23 @@
+package com.practice.problem01;
+
+import java.util.Scanner;
+
+public class Multiplication1692 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("곱셈할 수를 입력해주세요.");
+		int m = scan.nextInt();
+		System.out.print("");
+		int n = scan.nextInt();
+		
+		int h = n / 100;
+		int t = (n - h * 100) / 10;
+		int o = n % 10;
+		
+		System.out.println(m*o);
+		System.out.println(m*t);
+		System.out.println(m*h);
+		System.out.println("result: " + m*n);
+	}
+}
