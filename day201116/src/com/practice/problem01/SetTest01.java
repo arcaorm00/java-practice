baf5e6c8631ea0bee2181c7092018450919fa1d4
saltK <arcaorm00@gmail.com>
2020-12-08 00:19:11 +0900

    practiced set test

---
+++ day201116/src/com/practice/problem01/SetTest01.java
@@ -0,0 +1,20 @@
+package com.practice.problem01;
+
+import java.util.Random;
+import java.util.TreeSet;
+
+public class SetTest01 {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		TreeSet<Integer> ts = new TreeSet();
+		
+		while(ts.size() <= 6) {
+			int num = rand.nextInt(45)+1;
+			System.out.println(num);
+			ts.add(num);
+		}
+		
+		System.out.println(ts);
+	}
+}
