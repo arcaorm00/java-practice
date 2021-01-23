3b4235459401e8dda9278b8f9035275d86add781
saltK <arcaorm00@gmail.com>
2021-01-23 13:58:56 +0900

    practiced creating maven project

---
+++ day210116/src/main/java/com/web/test/service/BoardService.java
@@ -0,0 +1,8 @@
+package com.web.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+}
---
+++ day210116/src/main/java/com/web/test/service/BoardServiceImpl.java
@@ -0,0 +1,5 @@
+package com.web.test.service;
+
+public interface BoardServiceImpl {
+
+}
