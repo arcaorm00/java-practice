8b0082366d23332309a311d4a039d58969f0d07a
saltK <arcaorm00@gmail.com>
2021-02-15 21:52:26 +0900

    practiced creating security project

--- day210203_sec/src/main/java/com/controller/java/LoginController.java
+++ day210203_sec/src/main/java/com/controller/java/LoginController.java
@@ -10,10 +10,11 @@ public class LoginController {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 	
-	@RequestMapping(value="/login/login.do")
+	@RequestMapping(value="/login/loginPage.do")
 	public String login() {
 		return "/login/login";
 	}
+	
 	@RequestMapping(value="/login/accessDenied.do")
 	public String accessDeniedPage() throws Exception {
 		return "/login/accessDenied";
