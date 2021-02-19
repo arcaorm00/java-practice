d4b58e24ce1335fe98d73351f50b8f6a7cbd3c4b
saltK <arcaorm00@gmail.com>
2021-02-19 23:58:52 +0900

    practiced control flow statement

--- day201116/src/com/practice/problem02/ControlFlowStatement530.java
+++ day201116/src/com/practice/problem02/ControlFlowStatement530.java
@@ -6,7 +6,7 @@ public class ControlFlowStatement530 {
 	// 나이를 입력받아 20살 이상이면 "adult"라고 출력하고 그렇지 않으면 몇 년후에 성인이 되는지를 "○ years later"라는 메시지를 출력하는 프로그램을 작성하시오.
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("나이를 입력해주세요: ");
+		System.out.print("나이를 입력해주세요: ");
 		int age = Integer.parseInt(scan.next());
 		scan.close();
 		
