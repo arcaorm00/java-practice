0737ba15ea3c0edef3ef2d03281de4f176966e97
saltK <arcaorm00@gmail.com>
2021-02-24 23:58:29 +0900

    practiced control flow statement

--- day201116/src/com/practice/problem02/ControlFlowStatement124.java
+++ day201116/src/com/practice/problem02/ControlFlowStatement124.java
@@ -1,16 +1,17 @@
 package com.practice.problem02;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class ControlFlowStatement124 {
 	// 1~12사이의 정수를 입력받아 평년의 경우 입력받은 월을 입력받아 평년의 경우 해당 월의 날수를 출력하는 프로그램을 작성하시오.
 	public static void main(String[] args) {
 		int [] lastDay = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("마지막 날을 출력할 월을 입력해주세요.");
 		int month = scan.nextInt();
 		scan.close();
+		
 		System.out.println(lastDay[month-1]);
 	}
 }
