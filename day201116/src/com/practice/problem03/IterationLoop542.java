c82609bf77042ee702330c76fd5a701768ee1e96
saltK <arcaorm00@gmail.com>
2021-03-15 00:12:55 +0900

    practiced control iteration loop

---
+++ day201116/src/com/practice/problem03/IterationLoop542.java
@@ -0,0 +1,10 @@
+package com.practice.problem03;
+
+public class IterationLoop542 {
+	// 10부터 20까지의 숫자를 차례대로 출력하는 프로그램을 작성하시오.for문을 사용하세요.
+	public static void main(String[] args) {
+		for (int i = 10 ; i <= 20 ; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
