245bd251e6544074a12628dc37591383a3b9691c
saltK <arcaorm00@gmail.com>
2021-03-31 23:08:40 +0900

    practiced control iteration loop

---
+++ day201116/src/com/practice/problem03/IterationLoop551.java
@@ -0,0 +1,22 @@
+package com.practice.problem03;
+
+import java.util.Scanner;
+
+public class IterationLoop551 {
+	// 자연수 n을 입력받아서 다음과 같이 출력하는 프로그램을 작성하시오.
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		scan.close();
+		
+		for (int i = 0 ; i < input ; i++) {
+			for (int k = 0 ; k < i ; k++) {
+				System.out.print(" ");
+			}
+			for (int j = input ; j > i ; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
