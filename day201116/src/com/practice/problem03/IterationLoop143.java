c31d01aafb51755097673bf5809873c644732138
saltK <arcaorm00@gmail.com>
2021-04-09 23:13:50 +0900

    practiced control iteration loop

---
+++ day201116/src/com/practice/problem03/IterationLoop143.java
@@ -0,0 +1,30 @@
+package com.practice.problem03;
+
+import java.util.Scanner;
+
+public class IterationLoop143 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		scan.close();
+		
+		for (int i = 0; i < input; i++) {
+			for(int j = 0 ; j < i ; j++) {
+				System.out.print(" ");
+			}
+			for(int k = input*2 ; k > i*2+1 ; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 1 ; i < input ; i++) {
+			for (int j = i ; j < input ; j++) {
+				System.out.println(" ");
+			}
+			for (int k = 0 ; k < i ; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
