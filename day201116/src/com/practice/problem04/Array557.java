8680287c9fb221a4405a923aa7a75c5008938458
saltK <arcaorm00@gmail.com>
2021-04-26 00:49:46 +0900

    practiced array

---
+++ day201116/src/com/practice/problem04/Array557.java
@@ -0,0 +1,16 @@
+package com.practice.problem04;
+
+import java.util.Scanner;
+
+public class Array557 {
+	//10개의 문자를 입력받아서 첫 번째 네 번째 일곱 번째 입력받은 문자를 차례로 출력하는 프로그램을 작성하시오.
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		String [] array = input.split(" ");
+		
+		System.out.print(array[0] + " ");
+		System.out.print(array[3] + " ");
+		System.out.print(array[6] + " ");
+	}
+}
